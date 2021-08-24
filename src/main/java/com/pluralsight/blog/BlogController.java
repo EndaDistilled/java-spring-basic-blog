80b2db6dadabba80d25de4e12f9d3731423bf520
EndaDistilled <enda.brody@distilled.ie>
2021-08-24 11:20:55 +0100

    add step 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,23 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
     @RequestMapping
     public String listPosts(ModelMap map){
         map.put("title","Blog Post 1");
         return "home";
     }
+
+    public void PostRepository (PostRepository)
+        {
+            PostRepository = postRepository;
+
+        }
 
 }
