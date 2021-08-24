fd471a5ab9b2bf139dd187fa66f4c9f9eb06c9ed
EndaDistilled <enda.brody@distilled.ie>
2021-08-24 10:57:45 +0100

    typo

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
     @RequestMapping
-    public String listsPosts(ModelMap map){
+    public String listPosts(ModelMap map){
         map.put("title","Blog Posts 1");
         return "home";
     }
