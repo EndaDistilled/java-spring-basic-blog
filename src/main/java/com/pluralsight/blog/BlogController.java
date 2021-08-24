7336a3ca31bd5f02352749cd85b432cb77eda5cd
EndaDistilled <enda.brody@distilled.ie>
2021-08-24 10:55:55 +0100

    add method and add to page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,15 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+    @RequestMapping
+    public String listsPosts(ModelMap map){
+        map.put("title","Blog Posts 1");
+        return "home";
+    }
 
 }
